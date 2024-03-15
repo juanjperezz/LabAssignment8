d5750f01773559cedce954ce365d02ba1f761977
juanjperezz <160164366+juanjperezz@users.noreply.github.com>
2024-03-14 23:46:28 -0400

    sort.c

--- sort.c
+++ sort.c
@@ -31,6 +31,47 @@ size_t Size(void* ptr)
 // extraMemoryAllocated counts bytes of extra memory allocated
 void mergeSort(int pData[], int l, int r)
 {
+	if (l < r) {
+              int m = l + (r - l) / 2;
+              mergeSort(arr, l, m);
+
+              int n1 = m - l + 1;
+              int n2 = r - m;
+              int *L = (int *)Alloc(n1 * sizeof(int));
+              int *R = (int *)Alloc(n2 * sizeof(int));
+              for (int i = 0; i < n1; i++){
+                  L[i] = arr[l + i];
+              }
+              for (int j = 0; j < n2; j++){
+                  R[j] = arr[m + 1 + j];
+              }
+              int i = 0;
+              int j = 0;
+              int k = l;
+              while (i < n1 && j < n2) {
+                  if (L[i] <= R[j]){
+                      arr[k] = L[i];
+                      i++;
+                  } else {
+                      arr[k] = R[j];
+                      j++;
+                  }  
+                  k++;
+                  
+                  }
+                  while (i < n1){
+                      arr[k] = L[i];
+                      i++;
+                      k++; 
+                  }
+                  while (j < n2){
+                      arr[k] = R[j];
+                      j++;
+                      k++;
+                  }
+                  DeAlloc(L);
+                  DeAlloc(R);
+              }
 }
 
 // parses input file to an integer array
